7ed3952081351aa1ffee2ffc97683eb16a05c5e9
Overdo <g1005354641@gmail.com>
2017-01-27 08:20:16 +0800

    采集点信息界面

--- app/src/main/java/com/example/overdo/geoinfocollecter/activities/PointDetailActivity.java
+++ app/src/main/java/com/example/overdo/geoinfocollecter/activities/PointDetailActivity.java
@@ -3,6 +3,9 @@ package com.example.overdo.geoinfocollecter.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.overdo.geoinfocollecter.R;
@@ -10,6 +13,7 @@ import com.example.overdo.geoinfocollecter.entities.GeoInfo;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by Overdo on 2017/1/26.
@@ -31,6 +35,14 @@ public class PointDetailActivity extends BaseActivity {
     TextView mTvLat;
     @InjectView(R.id.tv_lng)
     TextView mTvLng;
+    @InjectView(R.id.ibtn_add_photo)
+    ImageButton mIbtnAddPhoto;
+    @InjectView(R.id.ibtn_add_pic)
+    ImageButton mIbtnAddPic;
+    @InjectView(R.id.btn_cancel)
+    Button mBtnCancel;
+    @InjectView(R.id.btn_confirm)
+    Button mBtnConfirm;
     private GeoInfo mGeoInfo;
 
 
@@ -50,10 +62,26 @@ public class PointDetailActivity extends BaseActivity {
         }
         mGeoInfo = (GeoInfo) intent.getSerializableExtra("geo_info");
         mTvDate.setText("日期：" + mGeoInfo.getDate());
-        mTvLocation.setText("地址："+ mGeoInfo.getAddress());
+        mTvLocation.setText("地址：" + mGeoInfo.getAddress());
         mTvLat.setText("latitude：" + mGeoInfo.getLatitude());
-        mTvLng.setText("longtitude：" +mGeoInfo.getLongtitude());
+        mTvLng.setText("longtitude：" + mGeoInfo.getLongtitude());
 
 
+    }
+
+    @OnClick({R.id.ibtn_add_photo, R.id.ibtn_add_pic, R.id.btn_cancel, R.id.btn_confirm})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ibtn_add_photo:
+                break;
+            case R.id.ibtn_add_pic:
+                break;
+            case R.id.btn_cancel:
+                finish();
+                break;
+            case R.id.btn_confirm:
+                //保存信息后finish
+                break;
+        }
     }
 }
