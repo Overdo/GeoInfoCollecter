a8396bbbb372fb27aac31a9610dd8216e80d4186
Overdo <g1005354641@gmail.com>
2017-01-27 08:40:24 +0800

    添加相片功能

--- app/src/main/java/com/example/overdo/geoinfocollecter/activities/PointDetailActivity.java
+++ app/src/main/java/com/example/overdo/geoinfocollecter/activities/PointDetailActivity.java
@@ -3,17 +3,27 @@ package com.example.overdo.geoinfocollecter.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.overdo.geoinfocollecter.R;
+import com.example.overdo.geoinfocollecter.adapter.PhotoAdapter;
 import com.example.overdo.geoinfocollecter.entities.GeoInfo;
+import com.example.overdo.geoinfocollecter.listener.RecyclerItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.iwf.photopicker.PhotoPicker;
+import me.iwf.photopicker.PhotoPreview;
 
 /**
  * Created by Overdo on 2017/1/26.
@@ -35,8 +45,6 @@ public class PointDetailActivity extends BaseActivity {
     TextView mTvLat;
     @InjectView(R.id.tv_lng)
     TextView mTvLng;
-    @InjectView(R.id.ibtn_add_photo)
-    ImageButton mIbtnAddPhoto;
     @InjectView(R.id.ibtn_add_pic)
     ImageButton mIbtnAddPic;
     @InjectView(R.id.btn_cancel)
@@ -46,6 +54,10 @@ public class PointDetailActivity extends BaseActivity {
     private GeoInfo mGeoInfo;
 
 
+    private PhotoAdapter photoAdapter;
+
+    private ArrayList<String> selectedPhotos = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +65,7 @@ public class PointDetailActivity extends BaseActivity {
         ButterKnife.inject(this);
 
         getIntentData();
+        initRecyclerView();
     }
 
     private void getIntentData() {
@@ -69,11 +82,9 @@ public class PointDetailActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.ibtn_add_photo, R.id.ibtn_add_pic, R.id.btn_cancel, R.id.btn_confirm})
+    @OnClick({ R.id.ibtn_add_pic, R.id.btn_cancel, R.id.btn_confirm})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.ibtn_add_photo:
-                break;
             case R.id.ibtn_add_pic:
                 break;
             case R.id.btn_cancel:
@@ -82,6 +93,63 @@ public class PointDetailActivity extends BaseActivity {
             case R.id.btn_confirm:
                 //保存信息后finish
                 break;
+        }
+    }
+
+    private void initRecyclerView(){
+
+        PhotoPicker.builder()
+                .setPhotoCount(9)
+                .setShowCamera(true)
+                .setShowGif(true)
+                .setPreviewEnabled(false)
+                .start(this, PhotoPicker.REQUEST_CODE);
+
+        RecyclerView rv_photoes = (RecyclerView) findViewById(R.id.rv_photoes);
+        photoAdapter = new PhotoAdapter(this, selectedPhotos);
+
+        rv_photoes.setLayoutManager(new StaggeredGridLayoutManager(4, OrientationHelper.VERTICAL));
+        rv_photoes.setAdapter(photoAdapter);
+
+        rv_photoes.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        if (photoAdapter.getItemViewType(position) == PhotoAdapter.TYPE_ADD) {
+                            PhotoPicker.builder()
+                                    .setPhotoCount(PhotoAdapter.MAX)
+                                    .setShowCamera(true)
+                                    .setPreviewEnabled(false)
+                                    .setSelected(selectedPhotos)
+                                    .start(PointDetailActivity.this);
+                        } else {
+                            PhotoPreview.builder()
+                                    .setPhotos(selectedPhotos)
+                                    .setCurrentItem(position)
+                                    .start(PointDetailActivity.this);
+                        }
+                    }
+                }));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK &&
+                (requestCode == PhotoPicker.REQUEST_CODE || requestCode == PhotoPreview.REQUEST_CODE)) {
+
+            List<String> photos = null;
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+            }
+            selectedPhotos.clear();
+
+            if (photos != null) {
+                selectedPhotos.addAll(photos);
+            }
+            photoAdapter.notifyDataSetChanged();
         }
     }
 }
